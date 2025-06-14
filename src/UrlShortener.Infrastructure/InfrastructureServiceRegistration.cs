09cf4c4fcc4397823e45e62570ce87c3448b78f3
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-14 22:03:59 +0400

    add infrastructure services

---
+++ src/UrlShortener.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UrlShortener.Application.Contracts.Infrastructure;
+using UrlShortener.Infrastructure.Cassandra;
+using UrlShortener.Infrastructure.Repositories;
+
+namespace UrlShortener.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<CassandraSettings>(configuration.GetSection("Cassandra"));
+
+            services.AddSingleton<CassandraDbContext>();
+
+            services.AddScoped<IUrlRepository, UrlRepository>();
+            services.AddScoped<IClickAnalyticsRepository, ClickAnalyticsRepository>();
+
+            services.AddHostedService<CassandraMigrationService>();
+            return services;
+        }
+    }
+}
