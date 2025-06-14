7f477a5bc3351e06587b41b034871d56b3f5b3e4
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-14 22:05:58 +0400

    add repository interfaces

---
+++ src/UrlShortener.Application/Contracts/Infrastructure/IClickAnalyticsRepository.cs
@@ -0,0 +1,10 @@
+﻿using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Application.Contracts.Infrastructure
+{
+    public interface IClickAnalyticsRepository
+    {
+        Task<IEnumerable<ClickAnalytics>> GetClickAnalyticsByShortCode(string shortCode);
+        Task AddAsync(ClickAnalytics clickAnalytics);
+    }
+}
---
+++ src/UrlShortener.Application/Contracts/Infrastructure/IUrlRepository.cs
@@ -0,0 +1,14 @@
+﻿using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Application.Contracts.Infrastructure
+{
+    public interface IUrlRepository
+    {
+        Task<Url?> GetByShortCodeAsync(string shortCode);
+        Task<Url?> GetByOriginalUrlAsync(string originalUrl);
+        Task IncreaseClickCount(string shortCode);
+        Task AddAsync(Url url);
+        Task UpdateAsync(Url url);
+        Task DeleteAsync(string shortCode);
+    }
+}
