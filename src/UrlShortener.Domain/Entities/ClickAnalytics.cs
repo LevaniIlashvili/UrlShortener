06f8dd0c4e68f67538de6ba4b479b6ae2d05c003
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-14 09:49:52 +0400

    add domain entities

---
+++ src/UrlShortener.Domain/Entities/ClickAnalytics.cs
@@ -0,0 +1,10 @@
+﻿namespace UrlShortener.Domain.Entities
+{
+    public class ClickAnalytics
+    {
+        public string ShortCode { get; set; }
+        public DateTime ClickDate { get; set; }
+        public string UserAgent { get; set; }
+        public string IpAddress { get; set; }
+    }
+}
---
+++ src/UrlShortener.Domain/Entities/Url.cs
@@ -0,0 +1,12 @@
+﻿namespace UrlShortener.Domain.Entities
+{
+    public class Url
+    {
+        public string ShortCode { get; set; }
+        public string OriginalUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public long ClickCount { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
