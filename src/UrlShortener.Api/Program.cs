d2611edef3c067dbabd544b668dbbccffa0a14b8
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-14 22:26:39 +0400

    map controllers

--- src/UrlShortener.Api/Program.cs
+++ src/UrlShortener.Api/Program.cs
@@ -2,6 +2,8 @@ using UrlShortener.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -17,5 +19,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
