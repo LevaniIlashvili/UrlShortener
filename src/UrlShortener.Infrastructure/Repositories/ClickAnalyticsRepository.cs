9fe4935bb2c221c1aa0b1234c5f0025c2281113e
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-14 22:07:38 +0400

    implement click analytics repository

---
+++ src/UrlShortener.Infrastructure/Repositories/ClickAnalyticsRepository.cs
@@ -0,0 +1,30 @@
+﻿using Cassandra;
+using Cassandra.Mapping;
+using UrlShortener.Application.Contracts.Infrastructure;
+using UrlShortener.Domain.Entities;
+using UrlShortener.Infrastructure.Cassandra;
+
+namespace UrlShortener.Infrastructure.Repositories
+{
+    public class ClickAnalyticsRepository : IClickAnalyticsRepository
+    {
+        private readonly ISession _session;
+        private readonly IMapper _mapper;
+
+        public ClickAnalyticsRepository(CassandraDbContext dbContext)
+        {
+            _session = dbContext.GetSession();
+            _mapper = new Mapper(_session);
+        }
+
+        public async Task AddAsync(ClickAnalytics clickAnalytics)
+        {
+            await _mapper.InsertAsync(clickAnalytics);
+        }
+
+        public async Task<IEnumerable<ClickAnalytics>> GetClickAnalyticsByShortCode(string shortCode)
+        {
+            return await _mapper.FetchAsync<ClickAnalytics>("SELECT * FROM click_analytics WHERE short_code = ?", shortCode);
+        }
+    }
+}
