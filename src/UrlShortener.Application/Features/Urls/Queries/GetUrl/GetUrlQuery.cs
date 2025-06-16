b63ea2059b199744f82b9dff0ce86d6365424bef
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-16 19:25:52 +0400

    implement query to get url

---
+++ src/UrlShortener.Application/Features/Urls/Queries/GetUrl/GetUrlQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Application.Features.Urls.Queries.GetUrl
+{
+    public class GetUrlQuery : IRequest<Url>
+    {
+        public string ShortCode { get; set; }
+        public string UserAgent { get; set; }
+        public string IpAddress { get; set; }
+    }
+}
---
+++ src/UrlShortener.Application/Features/Urls/Queries/GetUrl/GetUrlQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using UrlShortener.Application.Contracts.Infrastructure;
+using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Application.Features.Urls.Queries.GetUrl
+{
+    public class GetUrlQueryHandler : IRequestHandler<GetUrlQuery, Url>
+    {
+        private readonly IUrlRepository _urlRepository;
+        private readonly IClickAnalyticsRepository _clickAnalyticsRepository;
+
+        public GetUrlQueryHandler(IUrlRepository urlRepository, IClickAnalyticsRepository clickAnalyticsRepository)
+        {
+            _urlRepository = urlRepository;
+            _clickAnalyticsRepository = clickAnalyticsRepository;
+        }
+
+        public async Task<Url> Handle(GetUrlQuery request, CancellationToken cancellationToken)
+        {
+            var url = await _urlRepository.GetByShortCodeAsync(request.ShortCode);
+
+            if (url is null)
+            {
+                throw new Exceptions.NotFoundException("Url not found");
+            }
+
+            url.ClickCount++;
+            await _urlRepository.UpdateAsync(url);
+
+            var clickAnalytics = new ClickAnalytics()
+            {
+                ShortCode = url.ShortCode,
+                ClickDate = DateTime.UtcNow,
+                IpAddress = request.IpAddress,
+                UserAgent = request.UserAgent
+            };
+            await _clickAnalyticsRepository.AddAsync(clickAnalytics);
+
+            return url;
+        }
+    }
+}
