b7030b4ee8ca73d2a7a41def20184019bd941c0a
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-16 19:21:25 +0400

    add base62encoder

---
+++ src/UrlShortener.Application/Contracts/Infrastructure/IBase62Encoder.cs
@@ -0,0 +1,7 @@
+﻿namespace UrlShortener.Application.Contracts.Infrastructure
+{
+    public interface IBase62Encoder
+    {
+        string Encode(long number);
+    }
+}
---
+++ src/UrlShortener.Infrastructure/Services/Base62Encoder.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using UrlShortener.Application.Contracts.Infrastructure;
+
+namespace UrlShortener.Infrastructure.Services
+{
+    public class Base62Encoder : IBase62Encoder
+    {
+        private const string Base62Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        public string Encode(long number)
+        {
+            var result = new StringBuilder();
+
+            while (number > 0)
+            {
+                result.Insert(0, Base62Chars[(int)(number % 62)]);
+                number /= 62;
+            }
+
+            return result.ToString();
+        }
+    }
+}
