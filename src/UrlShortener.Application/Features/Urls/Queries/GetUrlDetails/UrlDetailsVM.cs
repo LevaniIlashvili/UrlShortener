d74694088fdb3a2d26843663038614e2ec16edb0
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-16 19:27:19 +0400

    create vm for url details

---
+++ src/UrlShortener.Application/Features/Urls/Queries/GetUrlDetails/UrlDetailsVM.cs
@@ -0,0 +1,15 @@
+﻿using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Application.Features.Urls.Queries.GetUrlDetails
+{
+    public class UrlDetailsVM
+    {
+        public string ShortCode { get; set; }
+        public string OriginalUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public long ClickCount { get; set; }
+        public bool IsActive { get; set; }
+        public List<ClickAnalytics> ClickAnalytics { get; set; }
+    }
+}
