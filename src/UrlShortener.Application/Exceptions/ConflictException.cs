d918c748d782659d7cda917365d3ec287be26efd
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-16 19:21:47 +0400

    add custom exceptions

---
+++ src/UrlShortener.Application/Exceptions/ConflictException.cs
@@ -0,0 +1,9 @@
+﻿namespace UrlShortener.Application.Exceptions
+{
+    public class ConflictException :  Exception
+    {
+        public ConflictException(string message) : base(message)
+        {  
+        }
+    }
+}
