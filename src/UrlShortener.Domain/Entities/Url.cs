5c52944ebb2aa079d1d2bd32936a98ee95110591
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-16 19:20:35 +0400

    make expiration date nullable

--- src/UrlShortener.Domain/Entities/Url.cs
+++ src/UrlShortener.Domain/Entities/Url.cs
@@ -5,7 +5,7 @@
         public string ShortCode { get; set; }
         public string OriginalUrl { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime ExpirationDate { get; set; }
+        public DateTime? ExpirationDate { get; set; }
         public long ClickCount { get; set; }
         public bool IsActive { get; set; }
     }
