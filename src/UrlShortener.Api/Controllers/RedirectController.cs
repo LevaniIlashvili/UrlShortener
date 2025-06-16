b2138968eb64f6a4edfe8d38df6ee87ee054932e
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-16 19:29:28 +0400

    create controller for url redirect

---
+++ src/UrlShortener.Api/Controllers/RedirectController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using UrlShortener.Application.Features.Urls.Queries.GetUrl;
+
+namespace UrlShortener.Api.Controllers
+{
+    [ApiController]
+    public class RedirectController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RedirectController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{shortCode}")]
+        public async Task<ActionResult> RedirectToOriginal(string shortCode)
+        {
+            var query = new GetUrlQuery
+            {
+                ShortCode = shortCode,
+                UserAgent = HttpContext.Request.Headers.UserAgent.ToString(),
+                IpAddress = HttpContext.Connection.RemoteIpAddress?.ToString()
+            };
+
+            var result = await _mediator.Send(query);
+
+            return Redirect(result.OriginalUrl);
+        }
+    }
+}
