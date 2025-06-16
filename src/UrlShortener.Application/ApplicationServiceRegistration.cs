9a6b58a0f80c4228d5cbb301694a04b3cac6c828
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-16 19:27:29 +0400

    register application services

---
+++ src/UrlShortener.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using UrlShortener.Application.Behaviors;
+using FluentValidation;
+
+namespace UrlShortener.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+
+            services.AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
