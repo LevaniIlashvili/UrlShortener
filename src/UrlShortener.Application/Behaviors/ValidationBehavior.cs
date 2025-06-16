a487f1988c6408e01b34da09b86b5dbe113644eb
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-16 19:26:40 +0400

    add validation pipeline for mediatr

---
+++ src/UrlShortener.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace UrlShortener.Application.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+        private readonly ILogger<ValidationBehavior<TRequest, TResponse>> _logger;
+     
+
+        public ValidationBehavior(
+            IEnumerable<IValidator<TRequest>> validators,
+            ILogger<ValidationBehavior<TRequest, TResponse>> logger)
+        {
+            _validators = validators;
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Count != 0)
+                {
+                    var requestName = typeof(TRequest).Name;
+                    _logger.LogWarning("Validation failed for request {RequestName}. Errors: {@Failures}", requestName, failures);
+                    throw new ValidationException(failures);
+                }
+
+            }
+
+            return await next();
+        }
+    }
+}
