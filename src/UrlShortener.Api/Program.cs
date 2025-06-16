7ad5e55c297de131ff291ed94402de734066d597
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-16 19:30:22 +0400

    register application services

--- src/UrlShortener.Api/Program.cs
+++ src/UrlShortener.Api/Program.cs
@@ -1,4 +1,5 @@
 using UrlShortener.Infrastructure;
+using UrlShortener.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
