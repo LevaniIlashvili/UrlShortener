850f77d183252349c8038a20726f2b4bd1056dc9
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-16 19:23:54 +0400

    implement command to delete url

---
+++ src/UrlShortener.Application/Features/Urls/Commands/DeleteUrl/DeleteUrlCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace UrlShortener.Application.Features.Urls.Commands.DeleteUrl
+{
+    public class DeleteUrlCommand : IRequest
+    {
+        public string ShortCode { get; set; } = "";
+    }
+}
---
+++ src/UrlShortener.Application/Features/Urls/Commands/DeleteUrl/DeleteUrlCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using UrlShortener.Application.Contracts.Infrastructure;
+
+namespace UrlShortener.Application.Features.Urls.Commands.DeleteUrl
+{
+    public class DeleteUrlCommandHandler : IRequestHandler<DeleteUrlCommand>
+    {
+        private readonly IUrlRepository _urlRepository;
+        private readonly IClickAnalyticsRepository _clickAnalyticsRepository;
+
+        public DeleteUrlCommandHandler(
+            IUrlRepository urlRepository,
+            IClickAnalyticsRepository clickAnalyticsRepository)
+        {
+            _urlRepository = urlRepository;
+            _clickAnalyticsRepository = clickAnalyticsRepository;
+        }
+
+        public async Task Handle(DeleteUrlCommand request, CancellationToken cancellationToken)
+        {
+            var url = await _urlRepository.GetByShortCodeAsync(request.ShortCode);
+
+            if (url is null)
+            {
+                throw new Exceptions.NotFoundException("Url not found");
+            }
+
+            await _urlRepository.DeleteAsync(url.ShortCode);
+            await _clickAnalyticsRepository.DeleteByShortCode(url.ShortCode);
+        }
+    }
+}
