a20a9ef6d1d525f4b94cd5457d6774cfd104f4c6
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-17 18:52:01 +0400

    create url expiration service

---
+++ src/UrlShortener.Infrastructure/Services/UrlExpirationService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+using UrlShortener.Application.Contracts.Infrastructure;
+
+namespace UrlShortener.Infrastructure.Services
+{
+    public class UrlExpirationService : BackgroundService
+    {
+        private readonly IUrlRepository _urlRepository;
+
+        public UrlExpirationService(IUrlRepository urlRepository)
+        {
+            _urlRepository = urlRepository;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await _urlRepository.DeactivateExpiredUrlsAsync();
+
+                await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
+            }
+        }
+    }
+}
