65942e3b7b6d23ce5057f935a96735fd5e370b3c
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-19 17:27:25 +0400

    add swagger ui

--- src/UrlShortener.Api/Program.cs
+++ src/UrlShortener.Api/Program.cs
@@ -7,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddApplicationServices();
@@ -18,7 +18,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger(); // Serves the OpenAPI JSON
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "UrlShortener API V1");
+        c.RoutePrefix = "swagger"; // This makes Swagger UI load at the root (http://localhost:5000/)
+    });
 }
 
 app.UseCustomExceptionHandler();
